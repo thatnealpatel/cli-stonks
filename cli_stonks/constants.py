49518658bb8b39ee7ef06b0bcbb96bf1a7057aa3
nealdotpy <nealpatel2021@gmail.com>
2020-12-15 18:03:47 -0500

    refactored T_BILL_SCRAPE_URL

--- cli_stonks/constants.py
+++ cli_stonks/constants.py
@@ -44,3 +44,4 @@ class Constants:
     TD_ENDPOINT_TOKEN = f'https://api.tdameritrade.com/v1/oauth2/token'
     TD_ENDPOINT_ACCOUNTS = f'https://api.tdameritrade.com/v1/accounts/{TDA_ID}?'
     TD_ENDPOINT_INSTRUMENTS = f'https://api.tdameritrade.com/v1/instruments?'
+    T_BILL_SCRAPE_URL = f'https://ycharts.com/indicators/3_month_t_bill'
