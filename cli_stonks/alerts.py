35d8747052f407809c5018ea0ce6934ecac9ca84
nealdotpy <nealpatel2021@gmail.com>
2021-02-25 23:21:17 -0500

    alarm for vix, air_raid for big poopie

--- cli_stonks/alerts.py
+++ cli_stonks/alerts.py
@@ -2,5 +2,7 @@ import os
 
 bell = 'assets/bell.mp3'
 scifi = 'assets/scifi_alarm.mp3'
+airraid = 'assets/air_raid.mp3'
 os.system('mpg123 -q ' + bell)
 os.system('mpg123 -q ' + scifi)
+os.system('mpg123 -q ' + airraid)
